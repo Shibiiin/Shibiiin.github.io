6d6556b719b801e2bd72dae44c15dc09cd2815bf
shibiiin <shibinm129@gmail.com>
2024-12-18 13:08:02 +0530

    build

--- lib/dock.dart
+++ lib/dock.dart
@@ -17,10 +17,10 @@ class Dock<T extends Object> extends StatefulWidget {
 
 /// State of the [Dock] used to manipulate the [_items].
 class _DockState<T extends Object> extends State<Dock<T>> {
-  late final List<T> _items = widget.items.toList();
-
-  T? _draggingItem;
-  T? _hoveredItem;
+  late List<T> _items = widget.items.toList();
+  T? _draggingItem; // Currently dragging item
+  T? _hoveredItem; // Item being hovered
+  bool _isDraggingOut = false;
 
   @override
   Widget build(BuildContext context) {
@@ -36,97 +36,96 @@ class _DockState<T extends Object> extends State<Dock<T>> {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           mainAxisSize: MainAxisSize.min,
-          children: _buildDraggableItems(),
+          children: _buildDraggableItems(w),
         ),
       ),
     );
   }
 
-  List<Widget> _buildDraggableItems() {
-    final w = MediaQuery.sizeOf(context).width;
-    bool isHover = false;
+  List<Widget> _buildDraggableItems(double containerWidth) {
     return List.generate(_items.length, (index) {
       final item = _items[index];
       final isHovered = _hoveredItem == item;
 
-      return AnimatedContainer(
-        height: 130,
-        duration: Duration(milliseconds: 200),
-        padding: EdgeInsets.only(
-            top: (isHover) ? 25 : 30, bottom: !(isHover) ? 25 : 30),
-        child: InkWell(
-          onHover: (value) {
-            print("Value $value");
-            setState(() {
-              isHover = value;
-            });
-          },
-          child: DragTarget<T>(
-            onWillAccept: (data) {
-              setState(() {
-                _hoveredItem = item;
-              });
-              return true;
-            },
-            onLeave: (data) {
-              setState(() {
-                _hoveredItem = null;
-              });
-            },
-            onAccept: (data) {
-              setState(() {
-                _items.remove(data);
-                _items.insert(index, data);
-                _hoveredItem = null;
-                _draggingItem = null;
-              });
-            },
-            builder: (context, candidateData, rejectedData) {
-              return AnimatedContainer(
-                duration: const Duration(milliseconds: 300),
-                padding: isHovered
-                    ? EdgeInsets.only(left: w * 0.06, right: w * 0.06)
-                    : const EdgeInsets.symmetric(horizontal: 4),
-                child: LongPressDraggable<T>(
-                  data: item,
-                  onDragStarted: () {
-                    setState(() {
-                      _draggingItem = item;
-                      print("Dragged $_draggingItem");
-                    });
-                  },
-                  onDraggableCanceled: (velocity, offset) {
-                    setState(() {
-                      _draggingItem = null;
-                      print("Dragged cancelled$_draggingItem");
-                    });
-                  },
-                  onDragEnd: (details) {
-                    if (details.wasAccepted == false) {
-                      setState(() {
-                        // _items.remove(item);
-                        _draggingItem = null;
-                      });
-                      print("Removed $item");
-                    }
-                  },
-                  feedback: Opacity(
-                    opacity: 0.7,
-                    child: Material(
-                      color: Colors.transparent,
-                      child: widget.builder(item),
-                    ),
-                  ),
-                  childWhenDragging: const SizedBox(
-                    width: 48,
-                    height: 48,
-                  ),
+      return DragTarget<T>(
+        onWillAccept: (data) {
+          setState(() {
+            _hoveredItem = item;
+            _isDraggingOut = false;
+          });
+          return true;
+        },
+        onLeave: (data) {
+          setState(() {
+            _hoveredItem = null;
+          });
+        },
+        onAccept: (data) {
+          setState(() {
+            _items.remove(data);
+            _items.insert(index, data);
+            _hoveredItem = null;
+            _draggingItem = null;
+          });
+        },
+        builder: (context, candidateData, rejectedData) {
+          return AnimatedContainer(
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.easeInOut,
+            padding: isHovered
+                ? EdgeInsets.only(
+                    left: containerWidth * 0.04, right: containerWidth * 0.04)
+                : const EdgeInsets.symmetric(horizontal: 4),
+            child: LongPressDraggable<T>(
+              data: item,
+              onDragStarted: () {
+                setState(() {
+                  _draggingItem = item;
+                  _isDraggingOut = false;
+                });
+              },
+              onDraggableCanceled: (velocity, offset) {
+                setState(() {
+                  _draggingItem = null;
+                  _isDraggingOut = false;
+                });
+              },
+              onDragEnd: (details) {
+                if (!_isDraggingOut) return;
+
+                setState(() {
+                  _items.remove(item);
+                  _draggingItem = null;
+                  _isDraggingOut = false;
+                });
+              },
+              feedback: Opacity(
+                opacity: 0.7,
+                child: Material(
+                  color: Colors.transparent,
                   child: widget.builder(item),
                 ),
-              );
-            },
-          ),
-        ),
+              ),
+              childWhenDragging: const SizedBox(
+                width: 10,
+                height: 10,
+              ),
+              child: MouseRegion(
+                onHover: (_) {
+                  setState(() {
+                    _hoveredItem = item;
+                  });
+                },
+                onExit: (_) {
+                  setState(() {
+                    _hoveredItem = null;
+                  });
+                },
+                child: widget.builder(item),
+              ),
+            ),
+          );
+        },
       );
     });
   }
